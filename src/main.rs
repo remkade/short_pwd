f029b7ec5c04f7e226dff98a5cb6c02e8838f994
Kyle Leaders <kyle.leaders@gmail.com>
2018-03-20 21:53:46 -0700

    Adding command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+#[macro_use]
+extern crate structopt;
+
 use std::env;
 use std::borrow::Cow;
+use structopt::StructOpt;
+
+/// A simple pwd shortener.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "short_pwd")]
+struct Opt {
+    /// The separator to replace the usual '/' with
+    #[structopt(short = "s", long = "separator", default_value="/")]
+    separator: String,
+}
 
 fn main() {
+    // Get all the necessary env vars
     let pwd = env::var_os("PWD")
         .expect("$PWD is not set in the environment!")
         .into_string()
@@ -10,7 +24,13 @@ fn main() {
         .expect("$HOME is not set in the environment!")
         .into_string()
         .expect("$HOME does not contain a valid string!");
-    println!("{}", remove_home(&home, &pwd));
+
+    // Parse commandline arguments
+    let opt = Opt::from_args();
+
+    let new_pwd = remove_home(&home, &pwd);
+
+    println!("{}", replace_separators(&opt.separator, &new_pwd));
 }
 
 fn remove_home<'a>(home: &str, pwd: &'a str) -> Cow<'a, str>{
@@ -18,6 +38,14 @@ fn remove_home<'a>(home: &str, pwd: &'a str) -> Cow<'a, str>{
         pwd.replace(home, "~").into()
     } else {
         pwd.into()
+    }
+}
+
+fn replace_separators<'a>(separator: &str, pwd: &'a str) -> Cow<'a, str>{
+    if separator == "/" {
+        pwd.into()
+    } else {
+        pwd.replace("/", separator).into()
     }
 }
 
@@ -31,5 +59,13 @@ mod tests {
         let pwd_without_home = "/home/potato/Documents";
         assert_eq!(remove_home(home, pwd_with_home), "~/Documents");
         assert_eq!(remove_home(home, pwd_without_home), pwd_without_home);
+    }
+
+    use replace_separators;
+    #[test]
+    fn replace_separators_test() {
+        let pwd = "/home/kyle";
+        assert_eq!(replace_separators("!", pwd), "!home!kyle");
+        assert_eq!(replace_separators("🤠", pwd), "🤠home🤠kyle");
     }
 }
